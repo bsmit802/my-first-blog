acb8ad57da8c34654fa40dd08e9c34a00a187d12
Beth <bethanyesmith95@gmail.com>
2020-05-05 15:07:29 +0800

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+#this folder is what django is directed to for further instructions for the different urls (pages on blog)
+
+from django.urls import path
+from . import views
+
+# we are assigning a view called post list to the root url. this will match an empty string and the django url resolver
+#will ignore the domain name that rpefixes the full url path. This tells django that views.post_list is the right place
+#to go if someone enters the website at the 'http://127.0.0.1:8000/' address. We have named the url post list
+urlpatterns = [
+    path('', views.post_list, name= 'post_list'),
+]

4f7ab720a6f4c909a70b9e98be6d51736ec67a3a
Beth <bethanyesmith95@gmail.com>
2020-05-11 13:33:26 +0800

    Added view and template for detailed blog post.

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,8 @@ from . import views
 # we are assigning a view called post list to the root url. this will match an empty string and the django url resolver
 #will ignore the domain name that rpefixes the full url path. This tells django that views.post_list is the right place
 #to go if someone enters the website at the 'http://127.0.0.1:8000/' address. We have named the url post list
+
 urlpatterns = [
     path('', views.post_list, name= 'post_list'),
+    path('post/<int:pk>/', views.post_detail, name= 'post_detail'),
 ]
